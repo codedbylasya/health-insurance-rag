ff02cb4554e7ce1bff00b6a1cfbde202358f986a
Lasya <inguva.lasya@gmail.com>
2026-07-14 15:44:13 -0500

    chore: remove test.py template file from src

--- src/test.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-embeddings = np.load(r"C:\Projects\PortfolioProject\health_Doc_Summarizer\data\embeddings.npy")
-print(embeddings.shape)
-print(embeddings.dtype)
-print(embeddings[0][:5])  # peek at first 5 values of the first vector
